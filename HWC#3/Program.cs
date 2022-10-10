0d150ba21791b9d2aec016834e78fc1d18d39291
Alesia <andomyel@yandex.ru>
2022-10-10 19:21:39 +0300

    HomeWork3task23(3right)

--- HWC#3/Program.cs
+++ HWC#3/Program.cs
@@ -1,20 +1,28 @@
 ﻿/*23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N*/
 
-/*Console.WriteLine("Введите число N");
+Console.WriteLine("Введите положительное число N ");
 int N = int.Parse(Console.ReadLine()!);
 int i = 1;
-while (i < N)
-{
-    double num = Math.Pow(i, 3);
-    Console.Write($"{num}, ");
-    i++;
+if (N > 0 ) {
+    while (i < N)
+    {
+        double num = Math.Pow(i, 3);
+        Console.Write($"{num}, ");
+        i++;
+    }
+    Console.WriteLine($"{Math.Pow(N, 3)}");
 }
-Console.WriteLine($"{Math.Pow(N, 3)}");*/
+else {
+    Console.WriteLine("Вы ввели отрицательное число или 0");
+}
+
+
+
 
 
 /*Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.*/
 
-Console.WriteLine("Введите координату AX");
+/*Console.WriteLine("Введите координату AX");
 int AX = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите координату AY");
@@ -34,4 +42,7 @@ int BZ = int.Parse(Console.ReadLine()!);
 
 double rezult = Math.Sqrt(Math.Pow(BX-AX, 2) + Math.Pow(BY-AY, 2) + Math.Pow(BZ-AZ, 2));
 
-Console.WriteLine($"Расстояние между точками A и B  в трехмерном пространстве {rezult:f2}");
+Console.WriteLine($"Расстояние между точками A и B  в трехмерном пространстве {rezult:f2}");*/
+
+
+//Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. (можно решить через строку, а можно математически)
